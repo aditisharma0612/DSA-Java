a8fdf9191c82e8444c0c1b058272c3651cd73354
aditisharma0612 <143523593+aditisharma0612@users.noreply.github.com>
2025-02-27 19:08:58 +0530

    Create subarray_count_with_given_sum.java

---
+++ subarray_count_with_given_sum.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Solution {
+    public static int findAllSubarraysWithGivenSum(int arr[], int k) {
+        int n = arr.length; // size of the given array.
+        int cnt = 0; // Number of subarrays:
+
+        for (int i = 0 ; i < n; i++) { // starting index i
+            for (int j = i; j < n; j++) { // ending index j
+
+                // calculate the sum of subarray [i...j]
+                int sum = 0;
+                for (int K = i; K <= j; K++)
+                    sum += arr[K];
+
+                // Increase the count if sum == k:
+                if (sum == k)
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 2, 4};
+        int k = 6;
+        int cnt = findAllSubarraysWithGivenSum(arr, k);
+        System.out.println("The number of subarrays is: " + cnt);
+    }
+}
