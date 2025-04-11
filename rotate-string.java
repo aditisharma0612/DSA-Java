dc55bfb7b56f4a12bc94f603565a411f409136d5
aditisharma0612 <143523593+aditisharma0612@users.noreply.github.com>
2025-04-11 17:38:10 +0530

    Create rotate-string.java

---
+++ rotate-string.java
@@ -0,0 +1,11 @@
+
+
+class Solution{
+    public boolean rotateString(String A, String B){
+        if(A.length()!=B.length()){
+
+            return false;
+        }
+        return (A+A).contains(B);
+    }
+}
