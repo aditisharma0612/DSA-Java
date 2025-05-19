49f848b2a03c3e419324e0bcc56845c1a0500824
aditisharma0612 <143523593+aditisharma0612@users.noreply.github.com>
2025-05-19 19:13:43 +0530

    Create BST-merge.java

---
+++ BST-merge.java
@@ -0,0 +1,29 @@
+class BSTIterator {
+    private TreeNode visit;
+    private Stack<TreeNode> stack;
+
+    public BSTIterator(TreeNode root) {
+        visit = root;
+        stack = new Stack();
+    
+        
+    }
+    
+    public int next() {
+         while (visit != null) {
+            stack.push(visit);
+            visit = visit.left;
+        }
+        TreeNode next = stack.pop();
+        visit = next.right;
+        return next.val;
+        
+    }
+    
+    public boolean hasNext() {
+         return visit != null || !stack.empty();
+    }
+
+        
+    
+}
