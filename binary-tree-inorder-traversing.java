a0ac95d0dd3e857a77ab2a0cc9b5f5e570cc2f1a
aditisharma0612 <143523593+aditisharma0612@users.noreply.github.com>
2025-05-22 18:19:36 +0530

    Create binary-tree-inorder-traversing.java

---
+++ binary-tree-inorder-traversing.java
@@ -0,0 +1,18 @@
+class Solution{
+    List<Integer>ans=new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inorder(root);
+        return ans;
+    }
+
+    public void inorder(TreeNode node){
+        if(node==null){
+            return;
+        }
+        inorder(node.left);
+        ans.add(node.val);
+        
+        inorder(node.right);
+    }
+
+}
