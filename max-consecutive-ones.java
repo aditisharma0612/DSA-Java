ecd5784c8eb17c0f7ad258bea21408da5be544b7
aditisharma0612 <143523593+aditisharma0612@users.noreply.github.com>
2025-05-24 18:06:24 +0530

    Create max-consecutive-ones.java

---
+++ max-consecutive-ones.java
@@ -0,0 +1,15 @@
+int findMaxConsecutiveOnes(vector<int>& nums) {
+        
+        int maxCon = INT_MIN;
+        int count = 0;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == 1) {
+                count++;
+            } else {
+                count = 0;
+            }
+            maxCon = max(maxCon, count);
+        }
+        return maxCon;
+    }
